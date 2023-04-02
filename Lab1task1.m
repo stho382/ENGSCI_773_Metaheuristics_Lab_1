d8689e00201df8805b76ae156e6f707ce515d5b3
Sebastian Thomas <stho382@aucklanduni.ac.nz>
2023-04-02 21:52:23 +1200

    updated_word_doc

--- Lab1task1.m
+++ Lab1task1.m
@@ -30,13 +30,13 @@ rng(233904926);
 % x = ga(fun, 2, [], [], [], [], [], [], [], options)
 
 %%%%%%%%%%%%%%%%% Simulated Annealing
-% x0 = [0 0];
-% fun = @griewank;
-% lb=[-50, -50];
-% ub = [50,50];
-% options = optimoptions('simulannealbnd', ...
-% 'PlotFcn', {@saplotbestf, @saplotf, @saplotbestx, @saplottemperature});
-% x = simulannealbnd(fun, x0, lb, ub, options)
+x0 = [0 0];
+fun = @griewank;
+lb=[-50, -50];
+ub = [50,50];
+options = optimoptions('simulannealbnd', ...
+'PlotFcn', {@saplotbestf, @saplotf, @saplotbestx, @saplottemperature});
+x = simulannealbnd(fun, x0, lb, ub, options)
 
 %%%%%%%%%%%%%%%%%% Particle Swarm
 % nvars = 2;
@@ -61,12 +61,12 @@ rng(233904926);
 % x = ga(fun, 2, [], [], [], [], [], [], [], options)
 
 %%%%%%%%%%%%%%%%% Particle swarm SelfAdjustmentWeight
-nvars = 2;
-fun = @multirosenbrock;
-lb=-10*ones(1, nvars);
-ub = -lb;
-options = optimoptions('particleswarm', 'PlotFcn', @pswplotbestf, SelfAdjustmentWeight=23);
-[x, fval] = particleswarm(fun, nvars, lb, ub, options)
+% nvars = 2;
+% fun = @multirosenbrock;
+% lb=-10*ones(1, nvars);
+% ub = -lb;
+% options = optimoptions('particleswarm', 'PlotFcn', @pswplotbestf, SelfAdjustmentWeight=23);
+% [x, fval] = particleswarm(fun, nvars, lb, ub, options)
 
 
 figure(1)
